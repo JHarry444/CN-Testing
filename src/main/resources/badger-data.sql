1fc52ce943827593e6e7d3fe7f5d072c2804d3d2
jharry444 <jordan.harrison@qa.com>
2020-10-27 13:15:17 +0000

    added comments

--- src/main/resources/badger-data.sql
+++ src/main/resources/badger-data.sql
@@ -1,2 +1,3 @@
 INSERT INTO `badger` (`name`, `age`, `habitat`)
 VALUES ('Barry', 26, 'Chase');
+-- H2 databases arre SUPER finnicky, you need to be careful to use '' for values and `` for field names.
--- src/test/java/com/qa/cne/rest/BadgerIntegrationTest.java
+++ src/test/java/com/qa/cne/rest/BadgerIntegrationTest.java
@@ -19,20 +19,31 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.cne.persistence.domain.Badger;
 
+//Launches the Spring context (on a random port to avoid conflicts)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
+//Automatically configures the MockMvc object we'll use to perform our tests
 @AutoConfigureMockMvc
+
+//Executes the schema and data files to reset the Badger table BEFORE each test
 @Sql(scripts = { "classpath:badger-schema.sql",
 		"classpath:badger-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 public class BadgerIntegrationTest {
 
 	@Autowired
+	// Object provided by the Spring testing dependency to allow for easy
+	// integration testing
 	private MockMvc mockMVC;
 
 	@Autowired
+	// Object Spring uses to convert objects sent from/to the @Controller's to and
+	// from JSON
 	private ObjectMapper mapper;
 
 	@Test
@@ -42,14 +53,38 @@ public class BadgerIntegrationTest {
 
 	@Test
 	void testCreate() throws Exception {
+		// Create the request body
 		Badger testBadger = new Badger("Bodger", 31, "BBC");
+		// Convert request body to JSON
 		String badgerAsJSON = this.mapper.writeValueAsString(testBadger);
 
+		// Create EXPECTED response body
 		Badger expectedBadger = new Badger(2L, testBadger.getName(), testBadger.getAge(), testBadger.getHabitat());
+		// Convert it to JSON
 		String excpectedBadgerAsJSON = this.mapper.writeValueAsString(expectedBadger);
 
-		this.mockMVC.perform(post("/create").content(badgerAsJSON).contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isCreated()).andExpect(content().json(excpectedBadgerAsJSON));
+		// I've broken the next bit apart so I can comment each part seperately
+
+		// BUILD THE TEST REQUEST
+		// Method = POST
+		// URL = /create
+		// body = badgerAsJSON
+		// headers = { "content-type": "application/json" }
+		RequestBuilder request = post("/create").content(badgerAsJSON).contentType(MediaType.APPLICATION_JSON);
+
+		// TEST THE STATUS
+		// For a create method it should be 201 (CREATED)
+		ResultMatcher checkStatus = status().isCreated();
+
+		// TEST THE RESPONSE BODY
+		// Should match excpectedBadgerAsJSON
+		ResultMatcher checkBody = content().json(excpectedBadgerAsJSON);
+
+		// PERFORM THE TEST REQUEST
+		// Sends the POST request we created earlier
+		// Checks the response has the correct status code
+		// Checks the response body matches our expected badger.
+		this.mockMVC.perform(request).andExpect(checkStatus).andExpect(checkBody);
 	}
 
 	@Test
